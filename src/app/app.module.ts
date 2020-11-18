659ea21bce9aaeb18865ea8b5d26b5fda690f853
ramphy0x90 <ramphy11@hotmail.com>
2020-11-18 22:48:14 +0100

    Login password check V1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,12 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, FormsModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/container/login/login.component.ts
+++ src/app/container/login/login.component.ts
@@ -1,14 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import {AlertComponent} from '../../component/alert/alert.component';
+import {ToastController} from '@ionic/angular';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss'],
 })
+
 export class LoginComponent implements OnInit {
+    private toastCtrl = new ToastController();
 
-  constructor() { }
+    currentPassword: number | string = '1234';
+    passwordIn: number | string;
 
-  ngOnInit() {}
+    constructor() {
+        /*const sqlite = new SQLite();
+        const test: Test = new Test(sqlite);
 
+        test.test();*/
+    }
+
+    ngOnInit() {}
+
+    setPassword() {
+    }
+
+    checkPassword() {
+        if (this.currentPassword == this.passwordIn) {
+            new AlertComponent(this.toastCtrl, "", "Nice");
+        }else{
+            new AlertComponent(this.toastCtrl, "", "Wrong password");
+        }
+    }
 }
