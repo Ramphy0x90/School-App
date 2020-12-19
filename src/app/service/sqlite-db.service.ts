25f6b9651f00f72cde2cdbc8f88c7beb16ddbdd2
ramphy0x90 <ramphy11@hotmail.com>
2020-12-19 14:18:42 +0100

    Sqlite service v2

--- src/app/service/sqlite-db.service.ts
+++ src/app/service/sqlite-db.service.ts
@@ -1,62 +1,97 @@
 import {Injectable} from '@angular/core';
 import {SQLite, SQLiteObject} from '@ionic-native/sqlite/ngx';
-import {Platform} from '@ionic/angular';
 import {HttpClient} from '@angular/common/http';
 import {SQLitePorter} from '@ionic-native/sqlite-porter/ngx';
-import {BehaviorSubject} from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
 })
 
 export class SqliteDbService {
-    private sqliteObj: SQLiteObject;
-    private isDbReady: BehaviorSubject<boolean> = new BehaviorSubject(false);
-    private tableName = 'credentials';
+    public dbObj;
+    public tmpNumRows;
 
     constructor(
-        private platform: Platform,
         private sqlite: SQLite,
         private httpClient: HttpClient,
         private sqlPorter: SQLitePorter
     ) {
-        this.platform.ready().then(() => {
-            this.createDb();
-        });
+        this.initDb();
     }
 
-    getDbStatus(){
-        return this.isDbReady.getValue();
-    }
-
-    createDb(){
-        this.sqlite.create({name: 'data.db', location: 'default'})
+    private initDb() {
+        this.sqlite.create({name: 'SA_db', location: 'default'})
             .then((db: SQLiteObject) => {
-                this.sqliteObj = db;
-                this.populateDb(this.sqliteObj);
-            })
-            .catch(e => {
-            console.log(e);
-        });
+                this.dbObj = db;
+                this.createStructure();
+            });
     }
 
-    populateDb(database){
+    private createStructure() {
         this.httpClient.get(
             'assets/data.sql',
             {responseType: 'text'}
         ).subscribe(data => {
-            this.sqlPorter.importSqlToDb(database, data)
-                .then(_ => {
-                    this.isDbReady.next(true);
-                    this.getOnce(1).then(r => console.log(r))
-                })
-                .catch(error => console.error(error));
+            this.sqlPorter.importSqlToDb(this.dbObj, data).then(r => {
+                console.log("Structure ready");
+            });
         });
     }
 
-    getOnce(id): Promise<String> {
-        return this.sqliteObj.executeSql(`SELECT * FROM ${this.tableName} WHERE id = ?`, [id]).then(res => {
-            return res.rows.item(0).password
-        });
+    getNumRows(tableName: String){
+        this.dbObj.executeSql(`SELECT COUNT(*) AS count FROM ${tableName}`, [])
+            .then(res => {
+                this.tmpNumRows = res.rows.item(0).count;
+                console.log("AUASID");
+                console.log(this.tmpNumRows);
+            })
+            .catch(errorLastId => console.log(errorLastId));
+
+        console.log(this.tmpNumRows)
+    }
+
+    getLastId(tableName: String): number{
+        let lastId: number;
+
+        this.dbObj.executeSql(`SELECT MAX(id) FROM ${tableName}`, [])
+            .then(res => {
+                lastId = res.rows.item(0).id;
+            })
+            .catch(errorLastId => console.log(errorLastId));
+
+        return lastId;
+    }
+
+    public setPassword(password: String){
+        if(this.tmpNumRows == 0){
+            this.dbObj.executeSql('INSERT INTO credentials(password) VALUES(?)', [password]);
+        }else{
+            let id = this.getLastId('credentials');
+            this.dbObj.executeSql('UPDATE credentials SET password = ? WHERE id = ?', [password, id]);
+        }
+
+    }
+
+    public exec(query: String, params: any[]) {
+        this.dbObj.executeSql(query, params)
+            .then()
+            .catch(errorQuery => {
+                console.log(errorQuery);
+            });
+    }
+
+    public getAll() {
+        this.dbObj.executeSql('SELECT * FROM credentials', [])
+            .then(res => {
+                let tmp = [];
+                for (let row = 0; row < res.rows.length; row++) {
+                    tmp.push({id: res.rows.item(row).id, password: res.rows.item(row).password});
+                }
+
+                console.log(tmp);
+            })
+            .catch(errorSelect => {
+               console.log('Select error: ' + errorSelect)
+            });
     }
 }
